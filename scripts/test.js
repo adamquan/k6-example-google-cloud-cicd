7f96e491c83f044d9bd575c542ce8125aa82bf9f
adamquan <adam.quan@gmail.com>
2022-04-06 00:58:00 -0500

    simple test

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,14 @@
-// ./test.js
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Rate } from "k6/metrics";
 
 export const options = {
-  duration: '1m',
-  vus: 50,
+  // Scale to 2 VUs over 10s, keep those 2 VUs for 20s and scale down over 10s
+  "stages": [
+    { "target": 2, "duration": "10s" }, // ramp-up
+    { "target": 2, "duration": "20s" }, // steady
+    { "target": 0, "duration": "10s" }  // ramp-down
+  ],
   thresholds: {
     http_req_duration: ['p(95)<500'],
   },
@@ -28,7 +32,29 @@ export const options = {
   },
 };
 
-export default function () {
-  const res = http.get('https://test.k6.io');
-  sleep(1);
+// K6 "Rate" metric for counting Javascript errors during a test run.
+var script_errors = Rate("script_errors");
+
+// Wraps a K6 test function with error counting.
+function wrapWithErrorCounting(fn) {
+  return (data) => {
+    try {
+      fn(data);
+      script_errors.add(0);
+    } catch (e) {
+      script_errors.add(1);
+      throw e;
+    }
+  }
 }
+
+// A very simple test
+function simpleTest() {
+  let response = http.get("https://test-api.k6.io", { "tags": { "name": "simple-test" } });
+  check(response, {
+    "200 OK": (r) => r.status === 200,
+  });
+  sleep(0.5);
+}
+
+export default wrapWithErrorCounting(simpleTest);
