339502217a08fa0eb1492371bfa362d2fbdc5740
adamquan <adam.quan@gmail.com>
2022-04-06 00:21:54 -0500

    added grafana cloud integration

--- scripts/test.js
+++ scripts/test.js
@@ -8,6 +8,24 @@ export const options = {
   thresholds: {
     http_req_duration: ['p(95)<500'],
   },
+  ext: {
+    loadimpact: {
+      apm: [
+        {
+          provider: 'prometheus',
+          remoteWriteURL: 'https://prometheus-prod-10-prod-us-central-0.grafana.net/api/prom/push',
+          credentials: {
+            username: '330312',
+            password: 'eyJrIjoiOWYzMmViNDMwNGMzZjM5ZDZjY2JiZTUwZDI4YTlmMDY3MTlkZGM3YSIsIm4iOiJhcXVhbi1lYXN5c3RhcnQtcHJvbS1wdWJsaXNoZXIiLCJpZCI6NjAxMzgwfQ==',
+          },
+          // optional parameters
+          metrics: ['http_req_duration', 'http_reqs', 'http_req_failed', 'vus', 'iterations', 'data_sent', 'data_received'], // ...
+          includeDefaultMetrics: true,
+          includeTestRunId: false,
+        },
+      ],
+    },
+  },
 };
 
 export default function () {
